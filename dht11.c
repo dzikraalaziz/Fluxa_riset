dda2602594fe604223fa24756310e2f09dcf42e6
Dzikra Al Aziz <dzayaziz1212@gmail.com>
2026-09-12 07:24:25 +0700

    memperbaiki codingan dht11

--- dht11.c
+++ dht11.c
@@ -1,66 +1,39 @@
-#include "driver/i2s.h"
+#include <DHT.h>
 
-#define I2S_WS 16
-#define I2S_SD 7
-#define I2S_SCK 15
+#define DHT_PIN 21
+#define DHT_TYPE DHT11
 
-#define I2S_PORT I2S_NUM_0
-#define bufferLen 64
-int16_t sBuffer[bufferLen];
-
-void i2s_install() {
-  const i2s_config_t i2s_config = {
-    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = 44100,
-    .bits_per_sample = i2s_bits_per_sample_t(16),
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
-    .intr_alloc_flags = 0,
-    .dma_buf_count = 8,
-    .dma_buf_len = bufferLen,
-    .use_apll = false
-  };
-  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
-}
-
-void i2s_setpin() {
-  const i2s_pin_config_t pin_config = {
-    .bck_io_num = I2S_SCK,
-    .ws_io_num = I2S_WS,
-    .data_out_num = -1,
-    .data_in_num = I2S_SD
-  };
-  i2s_set_pin(I2S_PORT, &pin_config);
-}
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  i2s_install();
-  i2s_setpin();
-  i2s_start(I2S_PORT);
-  delay(500);
+
+  dht.begin();
+
+  Serial.println("================================");
+  Serial.println("     TEST SENSOR DHT11");
+  Serial.println("     ESP32-S3 USB OTG");
+  Serial.println("================================");
 }
 
 void loop() {
-  int rangelimit = 3000;
-  Serial.print(rangelimit * -1);
-  Serial.print(" ");
-  Serial.print(rangelimit);
-  Serial.print(" ");
+  delay(2000);
 
-  size_t bytesIn = 0;
-  esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen * sizeof(int16_t), &bytesIn, portMAX_DELAY);
+  float suhu = dht.readTemperature();
+  float kelembapan = dht.readHumidity();
 
-  if (result == ESP_OK) {
-    int16_t samples_read = bytesIn / sizeof(int16_t);
-    if (samples_read > 0) {
-      float mean = 0;
-      for (int i = 0; i < samples_read; i++) {
-        mean += sBuffer[i];
-      }
-      mean /= samples_read;
-      Serial.println(mean);
-    }
+  if (isnan(suhu) || isnan(kelembapan)) {
+    Serial.println("Gagal membaca DHT11!");
+    return;
   }
+
+  Serial.print("Suhu       : ");
+  Serial.print(suhu);
+  Serial.println(" °C");
+
+  Serial.print("Kelembapan : ");
+  Serial.print(kelembapan);
+  Serial.println(" %");
+
+  Serial.println("-------------------------------");
 }
